b934ca484214f79ee118ba15974d6d740786c8bd
Gerardo Ulises Sanchez Felix <gerardosanchez@Gerardos-MacBook-Pro.local>
2025-08-06 15:47:20 -0600

    Do server-side actions and update the page, also change the color of the app to dark mode

--- windTurbineUiApp/src/ui/UiSdlApplicationStateWT.ts
+++ windTurbineUiApp/src/ui/UiSdlApplicationStateWT.ts
@@ -1,13 +1,28 @@
 import { setConfigInApplicationState } from '@c3/ui/UiSdlApplicationState';
 
-export function rowRecordStoreReducer(state, action) {
+export function rowRecordStoreReducer(state: any, action: any) {
   const appStateId = action.payload.componentId;
   return setConfigInApplicationState(appStateId, state, ['rowRecord'], action.payload.obj.id);
 }
 
-export function storeRowRecordAction(id, obj) {
+export function storeRowRecordAction(id: any, obj: any) {
   return {
     type: id + '.ROW_RECORD_STORE',
+    payload: {
+      componentId: id,
+      obj: obj
+    }
+  }
+}
+
+export function eventRecordStoreReducer(state: any, action: any) {
+  const appStateId = action.payload.componentId;
+  return setConfigInApplicationState(appStateId, state, ['eventToDelete'], action.payload.obj);
+}
+
+export function storeEventRecordAction(id: any, obj: any) {
+  return {
+    type: id + '.EVENT_RECORD_STORE',
     payload: {
       componentId: id,
       obj: obj
